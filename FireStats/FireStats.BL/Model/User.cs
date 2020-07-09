3118ef5dcd4d37cbbf780a553ee3433a74d322cd
Овчаренко Борис <akinfeev-34@mail.ru>
2020-07-09 19:51:02 +0300

    Добавил модель пользователя (свойства, конструктор) / Added user model (prop, ctor)

--- FireStats/FireStats.BL/Model/User.cs
+++ FireStats/FireStats.BL/Model/User.cs
@@ -3,14 +3,86 @@
 
 namespace FireStats.BL.Model
 {
+    /// <summary>
+    /// Пользователь.
+    /// </summary>
     public class User
     {
+        #region Свойства пользователя
+        /// <summary>
+        /// Имя пользователя (объекта).
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Тип пользователя ПСЧ/ЕДДС (обычный), ЦУКС (админ).
+        /// </summary>
         public UserType UserType { get; }
 
-        //параметры пользователя 
-        
+        /// <summary>
+        /// Адрес пользователя(объекта). Формат: ??? обл., г/д.???, ул/пер. ???, д. ???.
+        /// </summary>
+        public string Adress {get;}
+
+        /// <summary>
+        /// Количество личного состава.
+        /// </summary>
+        public int Personnel { get; set; }
+
+        /// <summary>
+        /// Количество боевой пожарной/специальной техники.
+        /// </summary>
+        public int FireTruck { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Создать нового пользователя.
+        /// </summary>
+        /// <param name="name">Имя.</param>
+        /// <param name="userType">Тип пользователя.</param>
+        /// <param name="adress">Адресс объекта.</param>
+        /// <param name="personnel">Личный состав.</param>
+        /// <param name="fireTruck">Боевая техника.</param>
+        public User(string name,
+                    UserType userType, 
+                    string adress, 
+                    int personnel,
+                    int fireTruck)
+        {
+            #region Проверка условий 
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя объекта не может быть пустым или null", nameof(name));
+            }
+            if (userType == null)
+            {
+                throw new ArgumentNullException("Тип пользователя не может быть null", nameof(userType));
+            }
+            if (string.IsNullOrWhiteSpace(adress))
+            {
+                throw new ArgumentNullException("Адрес объекта не может быть пустым или null", nameof(adress));
+            }
+            if (personnel <= 0)
+            {
+                throw new ArgumentException("Количество личного состава не может быть 0", nameof(personnel));
+            }
+            if (fireTruck <= 0)
+            {
+                throw new ArgumentException("Количество техники не может быть 0", nameof(fireTruck));
+            }
+            #endregion
+
+            Name = name;
+            UserType = userType;
+            Adress = adress;
+            Personnel = personnel;
+            FireTruck = fireTruck;            
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
 
     }
 }
