414795ce70b6d69c9977532dfd6e1e6072428e4a
Овчаренко Борис <akinfeev-34@mail.ru>
2020-08-17 14:33:29 +0300

    Добавил пакет Microsoft.Extensions

---
+++ FireStats/FireStats.WPF/Infrastructure/Converters/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace FireStats.WPF.Infrastructure.Converters
+{
+    internal abstract class Converter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider sp) => this;
+
+        public abstract object Convert(object v, Type t, object p, CultureInfo c);
+
+        public virtual object ConvertBack(object v, Type t, object p, CultureInfo c) => 
+            throw new NotSupportedException("Обратное преобразование не поддерживается");
+    }
+}
