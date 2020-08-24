8e7c479f70a4e5373a467aab8de4342e90cf0601
Овчаренко Борис <akinfeev-34@mail.ru>
2020-08-24 20:15:21 +0300

    TestData добавил отделения и пожарные автомобили.

--- FireStats/FireStats.WPF/Services/TestData.cs
+++ FireStats/FireStats.WPF/Services/TestData.cs
@@ -1,26 +1,26 @@
-﻿using FireStats.WPF.Models.Departments;
+﻿using FireStats.WPF.Models.Base;
+using FireStats.WPF.Models.Departments;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace FireStats.WPF.Services
 {
-    /// <summary>
-    /// Тестовые данные работников.
-    /// </summary>
+    /// <summary> Тестовые данные сотрудников и подразделений. </summary>
     static class TestData
     {
 
-
         public static Division[] Divisions { get; } = Enumerable
             .Range(1, 10)
-            .Select(i => new Division 
-            { 
+            .Select(i => new Division
+            {
                 Name = $"Подразделение {i}",
                 Employees = new ObservableCollection<Employee>(),
+                Units = new ObservableCollection<Unit>()
             })
             .ToArray();
 
@@ -33,9 +33,9 @@ namespace FireStats.WPF.Services
             var employees = new List<Employee>(100);
 
             foreach (var division in divisions)
-                for (var i = 0; i < 10; i++) 
+                for (var i = 0; i < 10; i++)
                 {
-                    
+
                     division.Employees.Add(new Employee
                     {
                         Name = $"Имя {employees_id}",
@@ -47,6 +47,34 @@ namespace FireStats.WPF.Services
                     });
                 }
             return divisions.SelectMany(d => d.Employees).ToArray();
+        }
+
+        public static Unit[] Units { get; } = CreateUnit(Divisions);
+
+        private static Unit[] CreateUnit(Division[] divisions)
+        {
+            var unit_id = 1;
+            var units = new List<Unit>(20);
+
+            foreach(var division in divisions)
+                for(var i=0; i<2;i++)
+                {
+                    division.Units.Add(new Unit
+                    {
+                        Active = true,
+                        InDivision = true,
+                        Truck = new Truck
+                        {
+                            Driver = division.Employees.First(),
+                            Crew = new ObservableCollection<Employee>() { division.Employees.Last() },
+                            Type = $"Автомобиль пожарный {unit_id}",
+                            Water = unit_id + 100,
+                            Foam = unit_id + 50,
+                            WaterSupply = 10* unit_id++
+                        }
+                    }); 
+                }
+            return divisions.SelectMany(u => u.Units).ToArray();
         }
     }
 }
